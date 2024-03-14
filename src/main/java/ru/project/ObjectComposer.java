0dc332542f9f04129042ae2d41356044f2428e35
Pavel <162188163+malipv@users.noreply.github.com>
2024-03-14 16:23:58 +0700

    Update ObjectComposer.java

--- src/main/java/ru/project/ObjectComposer.java
+++ src/main/java/ru/project/ObjectComposer.java
@@ -1,12 +1,27 @@
 package ru.project;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ObjectComposer {
     private Map<String, Object> objects = new HashMap<>();
     public ObjectComposer(String packageName) {
-        this.objects = objects;
+        //this.objects = objects;
+
+        try {
+            String filePackageName = '/' + packageName.replace('.', '/');
+            File f = new File("." + filePackageName);
+            for(File file : f.listFiles()) {
+                if (!file.getName().endsWith("class")) continue;
+                String clzName = file.getName().split("\\.")[0];
+                Class clz = Class.forName(packageName + "." + clzName);
+                if (!clz.isAnnotationPresent(Component.class)) continue;
+                objects.put(clzName, clz.newInstance());
+            }
+        } catch (Exception ex) {
+            throw new IllegalArgumentException(ex);
+        }
     }
     public Object getObjectByName(String name) {
         return objects.get(name);
