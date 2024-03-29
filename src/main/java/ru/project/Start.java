66a1a455a9635001d834755446d32a6b0ddccf6a
Pavel <162188163+malipv@users.noreply.github.com>
2024-03-29 15:38:58 +0700

    Инициализация бинов

--- src/main/java/ru/project/Start.java
+++ src/main/java/ru/project/Start.java
@@ -1,86 +1,113 @@
 package ru.project;
 
 import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.Comparator;
 
 public class Start {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext("ru.project");
-
-        // имена бинов - это имена классов, но с маленькой буквы
-        //System.out.println(ctx.getBean("config"));
-        //System.out.println(ctx.getBean("logic"));
-/*
-                ctx.getBean("operationMaker", OperationMaker.class)
-                .make();
-*/
-
-        Logic logic1 = ctx.getBean("logic", Logic.class);
-        Logic logic2 = ctx.getBean("logic", Logic.class);
-        System.out.println(logic1 == logic2);
-
+        ctx.close(); // полное уничтожение всех бинов в контейнере, перед полным уничтожением контейнера
     }
 }
 
-/*
-class MyScope implements org.springframework.beans.factory.config.Scope {
-
-    @Override
-    public Object get(String s, ObjectFactory<?> objectFactory) {
-        return null;
-    }
-}
-*/
-
-// настройка на java-config
-// создание бина выполняется путём вызова метода, помеченного словом Bean, у класса, помеченного аннотацией Configuraion
-@Configuration //- это тот же @Component
-// фабрика бинов
+@Configuration
 class Config {
 
-    Config(){
-        System.out.println("Create Config");
-    }
-
-    @Bean /*@Lazy*/ //@DependsOn("logic")
-        // фабричный метод
+    //@Bean
+    @Bean(initMethod = "toString")
     Logic logic2() {
-        System.out.println("Create logic from method");
-        /*
-        Logic ll = new Logic();
-        System.out.println("created");
-        //return new Logic();
-        return ll;
-        */
         return new Logic();
     }
+
+    @Bean
+    //Comparator<Integer> comparator() { return (x, y) -> x-y; }
+    //Comparator<Integer> comparator(Logic logic3) {
+    //Comparator<Integer> comparator(@Autowired(required = false) Start logic3) {
+        //System.out.println(logic3);
+    Comparator<Integer> comparator(Logic logic) {
+        System.out.println(logic);
+        return (x, y) -> x - y;
+    }
 }
 
-// настройка на аннотациях
-// вешаем на класс информацию, которая описывает, как объекты данного бина будут создаваться
-// через вызов конструктора
-
-@Component /*@Scope("singleton")*/ @Scope("prototype")
+@Component
 class Logic {
-    Logic(){
-        System.out.println("Create logic from class");
-    }
     void init() {
         System.out.println("finishing initialization " + this);
-
     }
 }
 
-@Component @DependsOn("logic2")
-class BLogic {
-    BLogic(){
-        System.out.println("Create logic from Bclass");
-    }
-    void init() {
-        System.out.println("finishing initialization " + this);
+//lookup
+@Component
+class ProjectComponent {
 
+
+    //@Autowired
+    //ApplicationContext ctx;
+
+    @Autowired
+    private Logic logic;
+
+    //List<Logic> lst;
+    Comparator<Integer> comparator;
+
+    @Autowired
+    private ProjectComponent() {
+        System.out.println("Creating empty");
+    }
+
+    public ProjectComponent(Logic logic) {
+        System.out.println("Creating with logic only");
+        //this.logic = logic;
+    }
+
+    //принудительное внедрение зависимости, по-умолчанию создаётся через конструктор
+    //@Autowired
+    public ProjectComponent(Logic logic, Comparator<Integer> comparator) {
+        System.out.println("Creating with both parameters");
+        //this.logic = logic;
+        this.comparator = comparator;
+    }
+
+    public Comparator<Integer> getComparator() {
+        return comparator;
+    }
+
+/*
+    @PostConstruct
+    void init() {
+        if (logic == null) {
+            ctx.getBean()
+        }
+    }
+*/
+
+/*
+    @Autowired
+    public void setLogic(Logic logic) {
+        System.out.println(this.logic);
+        this.logic = logic;
+    }
+*/
+    @Autowired
+    public void setComparator(Comparator<Integer> comparator) {
+        this.comparator = comparator;
+    }
+/*
+    @PostConstruct
+    public void init() {
+        logic.register(comparator);
+    }
+ */
+    @PreDestroy
+    public void dest() {
+        System.out.println("destroy");
     }
 }
